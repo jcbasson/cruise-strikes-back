9ce0110ded4b7bc203a79a7704f3339041bd29d2
Jan Basson <janbasson@Jans-MacBook-Air.local>
2022-05-05 22:33:41 +1000

    implement movies table

--- src/components/MovieList/components/MovieDataList.tsx
+++ src/components/MovieList/components/MovieDataList.tsx
@@ -1,4 +1,16 @@
-import { Heading } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Heading,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Text,
+} from "@chakra-ui/react";
 const MovieDataList = ({ movies }: { movies: any }) => {
   return (
     <main
@@ -10,24 +22,43 @@ const MovieDataList = ({ movies }: { movies: any }) => {
       }}
     >
       <Heading fontSize="xl">SEARCH RESULTS</Heading>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Actor</th>
-          </tr>
-        </thead>
-        {movies.map((movie: any) => {
-          return (
-            <tr>
-              <td>{movie.id}</td>
-              <td>{movie.title}</td>
-              <td>{movie.actor}</td>
-            </tr>
-          );
-        })}
-      </table>
+      <Box display="flex" alignItems="center">
+        <TableContainer>
+          <Table variant="simple" size="sm">
+            <Thead>
+              <Tr>
+                <Th></Th>
+                <Th>Title</Th>
+                <Th isNumeric>Year</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {movies.map((movie: any) => (
+                <Tr>
+                  <Td>
+                    <Avatar name={movie.title} src={movie.poster} />
+                  </Td>
+                  <Td>{movie.title}</Td>
+                  <Td isNumeric>{movie.year}</Td>
+                  <Td isNumeric>{movie.imdbRating}</Td>
+                  <Td>
+                    <Text noOfLines={[1, 2, 3]}>
+                      {movie.genres
+                        .reduce((prev: string, genre: { name: string }) => {
+                          if (genre && genre.name) {
+                            return `${prev},${genre.name}`;
+                          }
+                          return prev;
+                        }, "")
+                        .substring(1)}
+                    </Text>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
     </main>
   );
 };
